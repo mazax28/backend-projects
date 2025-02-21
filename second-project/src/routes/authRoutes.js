eafac2974cea9bdb43c9d06bcb0c46e5c2670600
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-02-21 14:36:00 -0300

    implement user registration and login endpoints with JWT authentication

--- second-project/src/routes/authRoutes.js
+++ second-project/src/routes/authRoutes.js
@@ -1,17 +1,56 @@
-import express from 'express';  // Importa el paquete Express para crear el servidor
-import bcrypt from 'bcryptjs';  // Importa el paquete bcrypt para encriptar contraseñas'
-import jwt from 'jsonwebtoken';  // Importa el paquete jsonwebtoken para crear tokens JWT
-import db from '../db.js';  // Importa la base de datos SQLite
+// authRoutes.js
+import express from 'express'; 
+import bcrypt from 'bcryptjs';  
+import jwt from 'jsonwebtoken';  
+import { insertUser, insertTodo, getUser } from '../db/dbQueries.js';  // Importa las funciones de consultas
 
-const router = express.Router();  // Crea un enrutador de Express
+const router = express.Router();  
 
-router.post('/register', (req,res)=>{
-    res.status(200).send('register')
-})
-router.post('/login',(req,res)=>{
-    res.status(200).send('login')
-})
+router.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 8);
+    
+    try {
+        // Usa la función insertUser desde dbQueries.js
+        const result = insertUser(username, hashedPassword);
+        
+        // Usa la función insertTodo para insertar la tarea por defecto
+        const defaultTodo = "Hello :) Add your first todo here!";
+        insertTodo(result.lastInsertRowid, defaultTodo);
+        
+        // Genera el token y responde
+        const token = jwt.sign({ id: result.lastInsertRowid }, process.env.JWT_SECRET, {
+            expiresIn: '24h'
+        });
 
+        res.json({ token });
 
+    } catch (error) {
+        console.error('Error during registration:', error);
+        res.status(503).json({ error: 'Service Unavailable', message: error.message });
+    }
+});
+
+router.post('/login', (req, res) => {
+    const {username, password} = req.body;
+    try{
+        const user = getUser(username);
+        if(!user){
+            return res.status(404).json({ error: 'User not Found' });
+        }
+        if(!bcrypt.compareSync(password, user.password)){
+            return res.status(401).json({ error: 'Invalid Password' });
+        }
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+            expiresIn: '24h'
+        }); 
+        res.json({ token });
+
+    }catch(error){
+        console.error('Error during login:', error);
+        res.status(503).json({ error: 'Service Unavailable', message: error.message });
+    }
+
+});
 
 export default router;
