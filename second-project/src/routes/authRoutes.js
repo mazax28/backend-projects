67ae762961915e74666c0f264e17bf0698270e2d
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-02-20 22:14:42 -0300

    add authRoutes.js for user registration endpoint

---
+++ second-project/src/routes/authRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';  // Importa el paquete Express para crear el servidor
+import bcrypt from 'bcryptjs';  // Importa el paquete bcrypt para encriptar contraseñas'
+import jwt from 'jsonwebtoken';  // Importa el paquete jsonwebtoken para crear tokens JWT
+import db from '../db.js';  // Importa la base de datos SQLite
+
+const router = express.Router();  // Crea un enrutador de Express
+
+router.post('/register', (req,res)=>{
+    res.status(200).send('register')
+})
+
+
+
+export default router;
