6de2729f537e8a3e00d1c31ffdb0d8de349b0d70
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-02-24 11:24:21 -0300

    add: integrate React Query for data fetching in main.jsx

--- pern-shop-crud/frontend/src/main.jsx
+++ pern-shop-crud/frontend/src/main.jsx
@@ -2,6 +2,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const client = new QueryClient()
+
 createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={client}>
     <App />
+  </QueryClientProvider>
 )
