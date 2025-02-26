1b3705fdec02a54591c296b654a7cfac3394c00b
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-02-25 22:30:26 -0300

    add: implement product listing with loading state and refresh functionality

--- pern-shop-crud/frontend/src/pages/HomePage.jsx
+++ pern-shop-crud/frontend/src/pages/HomePage.jsx
@@ -1,10 +1,59 @@
+import { useQuery, useQueryClient  } from "@tanstack/react-query";
+import { getProducts } from "../api/apiProducts";
+import { PlusCircleIcon, RefreshCwIcon } from "lucide-react";
+import ProductCard from "../components/ProductCard";
+import AddProductModal from "../components/AddProductModal";
 
 function HomePage() {
+  const queryClient = useQueryClient();
+  const { data: products, isLoading, error } = useQuery({
+    queryKey: ["products"],
+    queryFn: getProducts,
+  });
+
+  const handleRefresh = () => {
+    queryClient.invalidateQueries({ queryKey: ["products"] });
+  }
+
   return (
-    <div className="min-h-screen ">
-      Homepage
-    </div>
-  )
+    <main className="mx-auto px-4 py-8 max-w-6xl">
+      <div className="flex justify-between items-center mb-8">
+        <button className="btn btn-primary "  onClick={() => document.getElementById("add_product_modal").showModal()}> 
+          <PlusCircleIcon className="size-5 mr-2" />
+          Add Product
+        </button>
+        <button onClick={handleRefresh} className="btn btn-ghost btn-circle">
+          <RefreshCwIcon className="size-5" />
+        </button>
+      </div>
+      <AddProductModal />
+      {error && <div>Error: {error}</div>}
+
+      {
+        isLoading ? 
+        (
+          <div className="flex justify-center items-center h-64">
+              <div className="loading loading-spinner loading-lg"></div>
+          </div>
+        )
+        :
+        (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {
+              products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))
+            }
+
+          </div>
+        )
+      }
+      <div>
+
+      </div>
+
+    </main>
+  );
 }
 
-export default HomePage
+export default HomePage;
