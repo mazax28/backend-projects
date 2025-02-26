c6fdcf551bc5e8e435fca81d3a99dab5a94abf2f
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-02-25 22:32:13 -0300

    add: integrate React Query Devtools for enhanced debugging capabilities

--- pern-shop-crud/frontend/src/main.jsx
+++ pern-shop-crud/frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 
 import {
   QueryClient,
@@ -12,5 +14,6 @@ const client = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={client}>
     <App />
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 )
