fd1fcb677fa6323ab25c3db9b71d5c10859df0d2
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-02-25 22:28:50 -0300

    add: enable CORS for frontend access on localhost

--- pern-shop-crud/backend/src/server.js
+++ pern-shop-crud/backend/src/server.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 5000
 
 
+app.use(cors({
+    origin: 'http://localhost:5173' // URL de tu frontend
+  }));
+
+
+
 app.use(express.json())
+
 
 app.use('/auth', authRoutes);
 app.use('/products',productRoutes);
