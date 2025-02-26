f193de4fbaa9e79238e035b309e294f915bb34b4
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-02-25 22:32:21 -0300

    add: integrate theme management and toast notifications in App component

--- pern-shop-crud/frontend/src/App.jsx
+++ pern-shop-crud/frontend/src/App.jsx
@@ -2,19 +2,22 @@ import { BrowserRouter,Routes,Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import Navbar from "./components/Navbar"
 import ProductPage from "./pages/ProductPage"
+import { useThemeStore } from "./store/useThemeStore"
+import {Toaster} from 'react-hot-toast'
 
 function App() {
 
+  const {theme} = useThemeStore()
   return (
     <BrowserRouter>
-    <div className="min-h-screen bg-base-200 transition-colors duration-300">
+    <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme} >
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/product/:id" element={<ProductPage />} />
 
         </Routes>
-
+        <Toaster />
     </div>
 
     </BrowserRouter>
