a9cc145bb300cbaa5f15197aed4be4f009b4c4b4
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-02-27 15:38:18 -0300

    add: implement CRUD operations for Client in clientRoutes

---
+++ pern-client-crud/backend/src/routes/clientRoutes.js
@@ -0,0 +1,93 @@
+import express from 'express';
+import {prisma} from '../PrismaClient.js';
+const router = express.Router();
+
+
+router.get('/', async (req, res) => {
+    try{
+        const clients = await prisma.client.findMany();
+        res.json({clients});
+    }
+    catch (error){
+        res.status(500).json({error: 'Something went wrong'});
+    }
+});
+
+
+router.get('/:id', async (req, res) => {
+    const {id} = req.params;
+    try{
+        const client = await prisma.client.findUnique({
+            where: {
+                id: parseInt(id)
+            }
+        });
+        res.json({client});
+    }
+    catch (error){
+        res.status(500).json({error: 'Something went wrong'});
+    }
+});
+
+
+router.post('/', async (req, res) => {
+    console.log("Request body:", req.body); // <-- Agregar esto para verificar qué datos llegan al backend
+
+    const {name, email, Job, Rate, Status} = req.body;
+    try{
+        const newClient = await prisma.client.create({
+            data: {
+                name: name,
+                email: email,
+                Job: Job,
+                Rate: parseInt(Rate),
+                Status: Status
+            }
+        });
+        res.status(201).json({newClient});
+    }
+    catch (error){
+        res.status(500).json({error: 'Something went wrong'});
+    }
+})
+router.put('/:id', async (req, res) => {
+    const {name, email, Job, Rate, Status} = req.body;
+    console.log("Request body:", req.body); // <-- Agregar esto para verificar qué datos llegan al backend
+    console.log("Request params:", req.params); // <-- Agregar esto para verificar qué datos llegan al backend
+    const {id} = req.params;
+    try{
+        const updatedClient = await prisma.client.update({
+            where: {
+                id: parseInt(id)
+            },
+            data: {
+                name: name,
+                email: email,
+                Job: Job,
+                Rate: Rate,
+                Status: Status
+            }
+        });
+        res.json({updatedClient});
+    }
+    catch (error){
+        res.status(500).json({error: 'Something went wrong'});
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const {id} = req.params;
+    try{    
+        await prisma.client.delete({
+            where: {
+                id: parseInt(id)
+            }
+        });
+        res.json({message: 'Client deleted'});
+
+    }catch (error){
+        res.status(500).json({error: 'Something went wrong'});
+    }
+});
+
+export default router;
