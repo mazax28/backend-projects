24c2dde11cb71ba2468825abbb03e33db64502ae
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-02-27 15:39:12 -0300

    add: create mock data for clients in Clients.js

---
+++ pern-client-crud/frontend/src/mock/Clients.js
@@ -0,0 +1,83 @@
+export const Clients = [
+    {
+      "id": 1,
+      "name": "Juan Pérez",
+      "email": "juan.perez@example.com",
+      "Job": "Desarrollador",
+      "Rate": 5,
+      "status": true
+    },
+    {
+      "id": 2,
+      "name": "María Gómez",
+      "email": "maria.gomez@example.com",
+      "Job": "Diseñadora",
+      "Rate": 4,
+      "status": false
+    },
+    {
+      "id": 3,
+      "name": "Carlos Ramírez",
+      "email": "carlos.ramirez@example.com",
+      "Job": "Analista de datos",
+      "Rate": 5,
+      "status": true
+    },
+    {
+      "id": 4,
+      "name": "Ana López",
+      "email": "ana.lopez@example.com",
+      "Job": "Project Manager",
+      "Rate": 3,
+      "status": false
+    },
+    {
+      "id": 5,
+      "name": "Luis Torres",
+      "email": "luis.torres@example.com",
+      "Job": "Ingeniero de software",
+      "Rate": 5,
+      "status": true
+    },
+    {
+      "id": 6,
+      "name": "Sofía Herrera",
+      "email": "sofia.herrera@example.com",
+      "Job": "Tester",
+      "Rate": 4,
+      "status": true
+    },
+    {
+      "id": 7,
+      "name": "Pedro Sánchez",
+      "email": "pedro.sanchez@example.com",
+      "Job": "Administrador de sistemas",
+      "Rate": 3,
+      "status": false
+    },
+    {
+      "id": 8,
+      "name": "Lucía Fernández",
+      "email": "lucia.fernandez@example.com",
+      "Job": "Arquitecta de software",
+      "Rate": 5,
+      "status": true
+    },
+    {
+      "id": 9,
+      "name": "Ricardo Morales",
+      "email": "ricardo.morales@example.com",
+      "Job": "Consultor IT",
+      "Rate": 4,
+      "status": false
+    },
+    {
+      "id": 10,
+      "name": "Elena Castro",
+      "email": "elena.castro@example.com",
+      "Job": "Especialista en UX/UI",
+      "Rate": 4,
+      "status": true
+    }
+  ]
+  
