2a079dd74f3b3276b3def2ea51d723cce3f8c15b
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-03-12 22:54:06 -0300

    feat: add JWT utility for token generation and verification

---
+++ express-typescript/src/utils/jwtHandler.ts
@@ -0,0 +1,23 @@
+import {sign, verify} from 'jsonwebtoken';
+const JWT_SECRET = process.env.JWT_SECRET
+
+async function generateToken(id:string){
+    if (!JWT_SECRET){
+        throw new Error('JWT_SECRET not defined')
+    }
+    const jwt = sign({id},JWT_SECRET,{expiresIn: '1h'});
+    console.log(jwt);
+    return jwt;
+
+}
+
+function verifyToken(token:string){
+    if (!JWT_SECRET){
+        throw new Error('JWT_SECRET not defined')
+    }
+    const isOk = verify(token,JWT_SECRET);
+    return  isOk;
+
+}
+
+export {generateToken, verifyToken}
