e9033f883c6d7d87a162a18bc6d610c8eeeb862d
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-03-13 22:52:56 -0300

    feat: add logout route to authentication routes

--- express-typescript/src/routes/authRoutes.ts
+++ express-typescript/src/routes/authRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { loginCtrl, registerCtrl } from "../controllers/authController";
+import { loginCtrl, registerCtrl,logoutCtrl } from "../controllers/authController";
 
 const authRouter = Router();
 
 authRouter.post('/login',loginCtrl)
 authRouter.post('/register',registerCtrl)
+authRouter.post('/logout',logoutCtrl)
 
 export {authRouter}
