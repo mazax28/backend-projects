f83eccc227b1c3b5ce7dfd8d391f35f36ab1a61e
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-03-21 22:35:16 -0300

    fix: update comparePassword function parameters for clarity and consistency

--- auth-api/src/utils/encryptionHandler.ts
+++ auth-api/src/utils/encryptionHandler.ts
@@ -4,8 +4,8 @@ const hashPassword = async (password: string) =>{
     return await bcrypt.hash(password, 10);
 }
 
-const comparePassword = async (password: string, candidatePassword: string) => {
-    return await bcrypt.compare(candidatePassword, password);
+const comparePassword = async (candidatePassword: string, hashedPassword:string) => {
+    return await bcrypt.compare(candidatePassword, hashedPassword);
 }
 
 export {hashPassword, comparePassword};
