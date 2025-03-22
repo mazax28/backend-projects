3bc2367df5dac856d40f202d3bfb12da0fabfe04
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-03-22 15:39:23 -0300

    feat: add additional authentication routes for email verification and password recovery

--- auth-api/src/api/routes/authRoutes.ts
+++ auth-api/src/api/routes/authRoutes.ts
@@ -1,22 +1,16 @@
 import {Router} from 'express';
-import {registerCtlr,loginCtlr} from '../controllers/authController';
+import {registerCtlr,loginCtlr, verifyEmailCtlr,logoutCtlr,forgotPasswordCtlr,resetPasswordCtlr} from '../controllers/authController';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Auth API');
 
-})
 router.post('/login',loginCtlr);
 
 router.post('/register',registerCtlr)
 
-router.post('/logout', (req, res) => {
-
-    res.send('Logout');
-}
-);
-
-
+router.post('/logout', logoutCtlr)
+router.post('/verify-email', verifyEmailCtlr);
+router.post('/forgot-password', forgotPasswordCtlr);
+router.post('/reset-password/:token', resetPasswordCtlr);
 
 export {router};
