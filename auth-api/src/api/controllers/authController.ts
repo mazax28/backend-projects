f7b0206e807906027afbd40254db10c591dc0825
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-03-22 15:39:05 -0300

    feat: add email verification and password reset controllers

--- auth-api/src/api/controllers/authController.ts
+++ auth-api/src/api/controllers/authController.ts
@@ -1,8 +1,13 @@
 import type {Request, Response} from 'express';
 import { createUserSchema, loginUserSchema } from '../schemas/userSchema';
 import { handleError } from '../../utils/eventHandlers';
-import {registerService, loginService} from '../services/authService';
+import {registerService, loginService, verifyEmailService, resetPasswordService} from '../services/authService';
 import {sendVerificationEmail} from '../../mailtrap/email';
+import { verifyEmailTokenSchema } from '../schemas/authSchema';
+import { forgotPasswordSchema } from '../schemas/authSchema';
+import { forgotPasswordService } from '../services/authService';
+import { resetPasswordSchema } from '../schemas/authSchema';
+
 
 const registerCtlr = async (req: Request, res: Response) => {
     try {
@@ -36,4 +41,67 @@ const loginCtlr = async (req: Request, res: Response) => {
     }
 }
 
-export {registerCtlr, loginCtlr}
+const verifyEmailCtlr = async (req: Request, res: Response) => {
+    const { token } = req.body;
+    
+    try {
+        // Validar el token usando Zod
+        verifyEmailTokenSchema.parse({ token });
+
+        // Llamar al servicio para verificar el email
+        const result = await verifyEmailService(token);
+        
+        // Si la verificación es exitosa, enviar respuesta de éxito
+        if (result.success) {
+            return res.status(200).json({ success: true, message: 'Email verified successfully' });
+        }
+
+    } catch (error) {
+        // Manejo de errores
+        handleError(error as Error);
+        return res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+};
+const logoutCtlr = async (req: Request, res: Response) => {
+    try{
+        res.clearCookie('token');
+        res.status(200).json({message: 'Logout successful', success: true});
+    }
+    catch (error) {
+        handleError(error as Error);
+        res.status(500).send('Internal Server Error');
+    }
+
+}
+
+const  forgotPasswordCtlr = async (req: Request, res: Response) => {
+    try{
+        const {body} = req;
+        forgotPasswordSchema.parse(body);
+        const {email} = body;
+        await forgotPasswordService(email);
+        res.status(200).json({message: 'Password reset email sent', success: true});
+    }
+    catch (error) {
+        handleError(error as Error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+const resetPasswordCtlr = async (req: Request, res: Response) => {
+    try{
+        const {body} = req;
+        const {token} = req.params;
+        resetPasswordSchema.parse(body);
+        const {password} = body;
+        await resetPasswordService(token,password);
+        res.status(200).json({message: 'Password reset email sent', success: true});
+    }
+    catch (error) {
+        handleError(error as Error);
+        res.status(500).send('Internal Server Error');
+    }
+
+}
+
+export {registerCtlr, loginCtlr, verifyEmailCtlr, logoutCtlr, forgotPasswordCtlr,resetPasswordCtlr};
