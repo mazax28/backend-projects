b3acce9329ba4d82d1057305e8f95ee7211b375a
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-03-21 22:35:31 -0300

    feat: initialize server with database connection and event handling

--- auth-api/src/index.ts
+++ auth-api/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import {app} from './app';
+import {connectDB} from './config/db'
+import { handleInfo } from './utils/eventHandlers';
+const {PORT} = process.env;
+
+
+(async () => {
+    try {
+      await connectDB(); // Espera a que la conexión se establezca antes de iniciar el servidor
+      
+      app.listen(PORT, () => {
+        handleInfo(`Server is running on http://localhost:${PORT}`);
+      });
+    } catch (error) {
+      console.error('Failed to start server:', error);
+      process.exit(1);
+    }
+  })();
